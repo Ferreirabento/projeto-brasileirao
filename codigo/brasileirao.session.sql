d1031e0e57b70028b4c6d51573ecadbe5e0e1d05
Ferreirabento <jf6808932@gmai.com>
2026-01-22 10:20:59 -0300

    primeiro queria saber quais times e quantos estão sendo usados para essa analise

--- codigo/brasileirao.session.sql
+++ codigo/brasileirao.session.sql
@@ -1,1 +1,5 @@
-SELECT * from dataset_campeonato_br_2007_2024_v2;
+-- para saber quais times estão na tabela --
+select distinct Clube 
+from dataset_campeonato_br_2007_2024_v2
+where Campeao = 1
+order by Clube asc;
