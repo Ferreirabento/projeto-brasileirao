2bf7464e4642306c403d35bb5af31e02053ebd6a
Ferreirabento <jf6808932@gmai.com>
2026-01-22 10:22:36 -0300

    criei uma tabela para usar como amostra e separar da população

--- codigo/brasileirao.session.sql
+++ codigo/brasileirao.session.sql
@@ -3,3 +3,16 @@ select distinct Clube
 from dataset_campeonato_br_2007_2024_v2
 where Campeao = 1
 order by Clube asc;
+
+
+-- criação da tabela amostra para separar o que é importante --
+CREATE TABLE amostra (
+	ID INT primary KEY auto_increment,
+    ano int,
+    clube text,
+    receita_anual text,
+    qnt_jogadores int,
+	media_idade text,
+    estrangeiros int,
+    campeao int
+);
