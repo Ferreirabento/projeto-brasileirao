8045c18c5c51de53ec16b954b6092b74b2a54f39
Ferreirabento <jf6808932@gmai.com>
2026-01-26 16:24:20 -0300

    então cheguei a algum lugar.
    - tenho o primeiro bloco sendo ele a media para depois ver no desvio padrão se os dados estão muito enviesados
    
    - o segundo bloco era a moda para saber quais times mais venceram e descobri que foi o palmeiras, por isso estou usando ele
    
    - o terceiro bloco e da mediana para saber qual é o ponto central e depois ver no quartl
    
    basicmente, sinto que agora que começo meu projeto e o proximo passo talvez seja usar uma visualização em boxplot (talvez em python) para ter algo mais visual

--- codigo/brasileirao.session.sql
+++ codigo/brasileirao.session.sql
@@ -1,48 +1,48 @@
--- para saber quais times estão na tabela --
-select distinct Clube 
-from dataset_campeonato_br_2007_2024_v2
-where Campeao = 1
-order by Clube asc;
-
-
--- criação da tabela amostra para separar o que é importante --
-CREATE TABLE amostra (
-	ID INT primary KEY,
-    ano int,
-    clube text,
-    receita_anual text,
-    qnt_jogadores int,
-	media_idade text,
-    estrangeiros int,
-    campeao int
-);
-
-
--- inserindo os dados (amostragem não probabilistica) dos dados da população -- 
-insert into amostra (ano, clube, receita_anual, qnt_jogadores, media_idade, estrangeiros, campeao)
-Select Ano, Clube, Receita_anual, Qtd_Jogadores, Idade_Media, Estrangeiros, Campeao 
-from dataset_campeonato_br_2007_2024_v2
-order by Clube; 
-
-
--- aqui quero tentar estatistica para estudar --
--- mas primeiro vamos definir as perguntas --
--- quais foram os times que mais ganharam ? --
+-- aqui eu tenho as medias para depois fazer o desvio padrão 
 SELECT 
-FROM amostra
-WHERE campeao = 1
-ORDER BY clube;
-
-SELECT clube, count(*) as moda
-FROM amostra
-WHERE campeao = 1
-GROUP BY clube;
-
--- em qual ano ganaram ? --
-SELECT Clube, AVG(Vitorias), AVG(Empates), AVG (Derrotas), avg(Gols_feitos), AVG(Gols_sofridos), AVG(Receita_anual)
+    Clube, 
+    AVG(Vitorias), 
+    AVG(Empates), 
+    AVG(Derrotas), 
+    AVG(Gols_feitos), 
+    AVG(Gols_sofridos), 
+    AVG(Receita_anual)
 FROM dataset_campeonato_br_2007_2024_v2
 WHERE clube = 'Palmeiras' AND Campeao = 1
 ORDER BY ano;
 
--- o que tinham em comun com os outros times que ganharam ? --
--- o que falto nos outros times que não ganharam --
+
+-- aqui eu tenho a moda dos clubes que ganharam -- 
+SELECT 
+    clube, 
+    count(*) as moda
+FROM dataset_campeonato_br_2007_2024_v2
+WHERE campeao = 1
+GROUP BY clube;
+
+
+-- mediana para depois usar no quartil
+SELECT AVG(Vitorias) as mediana
+FROM (
+    SELECT Vitorias,
+        PERCENT_RANK() OVER(ORDER BY Vitorias) AS TESTE
+    FROM dataset_campeonato_br_2007_2024_v2
+    WHERE clube = 'Palmeiras'
+) AS subquery
+WHERE TESTE BETWEEN 0.45 AND 0.55;
+
+
+-- quartil --
+SELECT
+    clube,
+    Vitorias,
+    NTILE(4) over (ORDER BY Vitorias) as quartil
+FROM dataset_campeonato_br_2007_2024_v2
+WHERE clube = 'Palmeiras' AND Campeao = 1;
+
+
+-- aqui eu já tenho o desvio padrão --
+SELECT Clube, STDDEV(Vitorias), STDDEV(Empates), STDDEV(Derrotas), STDDEV(Gols_feitos), STDDEV(Gols_sofridos), STDDEV(Receita_anual)
+from dataset_campeonato_br_2007_2024_v2
+WHERE Clube = 'Palmeiras' and Campeao = 1;
+
